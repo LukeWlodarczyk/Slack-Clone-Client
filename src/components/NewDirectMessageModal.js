6d454a8600ff3b4d29ab838bacb4b4c047ed8c32
LukeWlodarczyk <wlod_a_rczyk@interia.pl>
2018-09-12 13:55:33 +0200

    disable submit button if no users selected to message

--- src/components/NewDirectMessageModal.js
+++ src/components/NewDirectMessageModal.js
@@ -1,125 +1,131 @@
-import React from 'react';
-import { Button, Form, Modal } from 'semantic-ui-react';
-import { Formik } from 'formik';
-import { Mutation } from 'react-apollo';
+import React from "react";
+import { Button, Form, Modal } from "semantic-ui-react";
+import { Formik } from "formik";
+import { Mutation } from "react-apollo";
 
-import MultiSelectUsers from './MultiSelectUsers';
+import MultiSelectUsers from "./MultiSelectUsers";
 
-import { GET_OR_CREATE_CHANNEL } from '../queries/channel';
-import { AUTH_USER } from '../queries/user';
-import formatApiErrors from '../helpers/formatApiErrors';
+import { GET_OR_CREATE_CHANNEL } from "../queries/channel";
+import { AUTH_USER } from "../queries/user";
+import formatApiErrors from "../helpers/formatApiErrors";
 
 const update = teamId => (store, { data: { getOrCreateChannel } }) => {
-	const { channel, success } = getOrCreateChannel;
+  const { channel, success } = getOrCreateChannel;
 
-	if (!success) {
-		return;
-	}
-	const data = store.readQuery({ query: AUTH_USER });
+  if (!success) {
+    return;
+  }
+  const data = store.readQuery({ query: AUTH_USER });
 
-	const newData = JSON.parse(JSON.stringify(data));
+  const newData = JSON.parse(JSON.stringify(data));
 
-	newData.getAuthUser.teams.find(team => {
-		if (team.id === teamId) {
-			team.channels = [...team.channels, channel];
-		}
-	});
+  newData.getAuthUser.teams.find(team => {
+    if (team.id === teamId) {
+      team.channels = [...team.channels, channel];
+    }
+  });
 
-	store.writeQuery({ query: AUTH_USER, data: newData });
+  store.writeQuery({ query: AUTH_USER, data: newData });
 };
 
 const DirectMessageModal = ({
-	open,
-	onClose,
-	teamId,
-	currentUserId,
-	history,
+  open,
+  onClose,
+  teamId,
+  currentUserId,
+  history
 }) => (
-	<Mutation mutation={GET_OR_CREATE_CHANNEL} update={update(teamId)}>
-		{getOrCreateChannel => (
-			<Formik
-				initialValues={{
-					privateMembers: [],
-				}}
-				onSubmit={async (values, { setSubmitting, setErrors, resetForm }) => {
-					const response = await getOrCreateChannel({
-						variables: {
-							dmMembers: values.privateMembers,
-							teamId,
-						},
-						optimisticResponse: {
-							getOrCreateChannel: {
-								__typename: 'Mutation',
-								success: true,
-								channel: {
-									__typename: 'Channel',
-									id: -1,
-									name: values.privateMembers.join(' ,'),
-									dm: true,
-								},
-								errors: null,
-							},
-						},
-					});
+  <Mutation mutation={GET_OR_CREATE_CHANNEL} update={update(teamId)}>
+    {getOrCreateChannel => (
+      <Formik
+        initialValues={{
+          privateMembers: []
+        }}
+        onSubmit={async (values, { setSubmitting, setErrors, resetForm }) => {
+          const response = await getOrCreateChannel({
+            variables: {
+              dmMembers: values.privateMembers,
+              teamId
+            },
+            optimisticResponse: {
+              getOrCreateChannel: {
+                __typename: "Mutation",
+                success: true,
+                channel: {
+                  __typename: "Channel",
+                  id: -1,
+                  name: values.privateMembers.join(" ,"),
+                  dm: true
+                },
+                errors: null
+              }
+            }
+          });
 
-					setSubmitting(false);
+          setSubmitting(false);
 
-					const { success, channel, errors } = response.data.getOrCreateChannel;
+          const { success, channel, errors } = response.data.getOrCreateChannel;
 
-					if (success || (!success && channel.id)) {
-						resetForm();
-						onClose();
-						return history.push(`/view-team/${teamId}/${channel.id}`);
-					}
+          if (success || (!success && channel.id)) {
+            resetForm();
+            onClose();
+            return history.push(`/view-team/${teamId}/${channel.id}`);
+          }
 
-					setErrors(formatApiErrors(errors));
-				}}
-				render={({
-					values,
-					errors,
-					touched,
-					handleSubmit,
-					isSubmitting,
-					resetForm,
-					setFieldValue,
-				}) => (
-					<Modal open={open} onClose={onClose}>
-						<Modal.Header>New conversation</Modal.Header>
-						<Modal.Content>
-							<Form onSubmit={handleSubmit}>
-								<Form.Field>
-									<MultiSelectUsers
-										teamId={teamId}
-										placeholder="Select members to message"
-										value={values.privateMembers}
-										handleChange={(e, { value }) =>
-											setFieldValue('privateMembers', value)
-										}
-										currentUserId={currentUserId}
-									/>
-								</Form.Field>
-								<Form.Group widths="equal">
-									<Button
-										disabled={isSubmitting}
-										fluid
-										onClick={e => {
-											resetForm();
-											onClose(e);
-										}}
-									>
-										Cancel
-									</Button>
-									<Button disabled={isSubmitting} type="submit" fluid>
-										Create conversation
-									</Button>
-								</Form.Group>
-							</Form>
-						</Modal.Content>
-					</Modal>
-				)}
-			/>
-		)}
-	</Mutation>
+          setErrors(formatApiErrors(errors));
+        }}
+        render={({
+          values,
+          errors,
+          touched,
+          handleSubmit,
+          isSubmitting,
+          resetForm,
+          setFieldValue
+        }) => (
+          <Modal open={open} onClose={onClose}>
+            <Modal.Header>New conversation</Modal.Header>
+            <Modal.Content>
+              <Form onSubmit={handleSubmit}>
+                <Form.Field>
+                  <MultiSelectUsers
+                    teamId={teamId}
+                    placeholder="Select members to message"
+                    value={values.privateMembers}
+                    handleChange={(e, { value }) =>
+                      setFieldValue("privateMembers", value)
+                    }
+                    currentUserId={currentUserId}
+                  />
+                </Form.Field>
+                <Form.Group widths="equal">
+                  <Button
+                    disabled={isSubmitting}
+                    fluid
+                    onClick={e => {
+                      resetForm();
+                      onClose(e);
+                    }}
+                  >
+                    Cancel
+                  </Button>
+                  <Button
+                    disabled={
+                      isSubmitting || values.privateMembers.length === 0
+                    }
+                    type="submit"
+                    fluid
+                  >
+                    Create conversation
+                  </Button>
+                </Form.Group>
+              </Form>
+            </Modal.Content>
+          </Modal>
+        )}
+      />
+    )}
+  </Mutation>
 );
 
 export default DirectMessageModal;
